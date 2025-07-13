9b0229321dfa62b8bca0f1af8b9f03fcf57546a8
Umherirrender <umherirrender_de.wp@web.de>
2025-07-13 10:57:08 +0200

    build: Updating mediawiki/mediawiki-phan-config to 0.16.0
    Change-Id: I1d4c9a44849bf217772f40838a4289859624bbdb

--- .phan/config.php
+++ .phan/config.php
@@ -1,8 +1,3 @@
 <?php
 
-$cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
-
-// This value should match the PHP version specified in composer.json and PHPVersionCheck.php.
-$cfg['minimum_target_php_version'] = '8.1.0';
-
-return $cfg;
+return require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';

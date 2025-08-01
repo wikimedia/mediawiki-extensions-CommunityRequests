1c4cc56f11dcbadc1cd4a2551f707bd03af3a63f
hmonroy <hmonroy@wikimedia.org>
2025-08-01 16:51:16 -0700

    Check if user is logged in when showing voting button
    Check if user is logged in when displaying the support button in Vue. If
    the user is not logged in, they should be redirected to the user login
    page.
    
    Bug: T400956
    Change-Id: I8bbe592b36a7cbcd3eb977dec260c929d4c4acaa

---
+++ modules/voting/AuthButton.vue
@@ -0,0 +1,32 @@
+<template>
+	<a
+		:href="userLoginUrl"
+		class="cdx-button
+			cdx-button--fake-button cdx-button--fake-button--enabled
+			cdx-button--action-progressive cdx-button--weight-primary"
+	>
+		{{ Util.isWishPage() ?
+			$i18n( 'communityrequests-wish-voting-not-logged-in' ).text() :
+			$i18n( 'communityrequests-focus-area-voting-not-logged-in' ).text()
+		}}
+	</a>
+</template>
+
+<script>
+const { defineComponent } = require( 'vue' );
+const Util = require( '../common/Util.js' );
+
+module.exports = exports = defineComponent( {
+	name: 'AuthVotingButton',
+	setup() {
+		const userLoginUrl = mw.util.getUrl(
+			'Special:UserLogin', { returnto: mw.config.get( 'wgPageName' ) }
+		);
+
+		return {
+			userLoginUrl,
+			Util
+		};
+	}
+} );
+</script>
--- modules/voting/init.js
+++ modules/voting/init.js
@@ -2,6 +2,12 @@
 const buttonRoot = document.querySelector( '.ext-communityrequests-voting-btn' );
 if ( buttonRoot ) {
 	const Vue = require( 'vue' );
-	const VotingButton = require( './Button.vue' );
+	let VotingButton = null;
+	if ( mw.user.isNamed() ) {
+		VotingButton = require( './Button.vue' );
+
+	} else {
+		VotingButton = require( './AuthButton.vue' );
+	}
 	Vue.createMwApp( VotingButton ).mount( buttonRoot );
 }

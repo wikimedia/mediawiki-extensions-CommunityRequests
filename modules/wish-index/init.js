1792faceb54d69ef82dd900a9096e47c2fd5e3d7
MusikAnimal <musikanimal@gmail.com>
2025-10-14 18:31:26 -0400

    wish-index: pass in wishesData so that initial filters are set
    Without this, focus area pages will list *all* wishes and not just those
    belonging to that focus area.
    
    Bug: T400945
    Follow-Up: Ifb39ea134e5fa1a8e0466543e4259a174a5ae93b
    Change-Id: I93a9222443e8e206374e3b18d920d8c9b50bd95e

--- modules/wish-index/init.js
+++ modules/wish-index/init.js
@@ -4,5 +4,5 @@ const root = document.querySelector( '.ext-communityrequests-wishes' );
 if ( root ) {
 	const Vue = require( 'vue' );
 	const WishIndex = require( './WishIndex.vue' );
-	Vue.createMwApp( WishIndex ).mount( root );
+	Vue.createMwApp( WishIndex, mw.config.get( 'wishesData' ) ).mount( root );
 }

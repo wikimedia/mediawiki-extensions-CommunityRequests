cf1805392f24e7e48a94c0afdb240d11189344d2
MusikAnimal <musikanimal@gmail.com>
2025-11-06 20:58:17 -0500

    ChangesListHooks: show entity titles in recent changes and watchlists
    Move entity linking code from SearchHooks into WishlistEntityTrait.
    
    Bug: T406957
    Change-Id: Idad9d4a6ca349acc9a83dd3d336220edb7ebac74
    (cherry picked from commit 14a521a12d218b7d9a26a4100e6b3233598fd3bd)

---
+++ includes/HookHandler/ChangesListHooks.php
@@ -0,0 +1,57 @@
+<?php
+declare( strict_types = 1 );
+
+namespace MediaWiki\Extension\CommunityRequests\HookHandler;
+
+use MediaWiki\Extension\CommunityRequests\FocusArea\FocusAreaStore;
+use MediaWiki\Extension\CommunityRequests\Wish\WishStore;
+use MediaWiki\Extension\CommunityRequests\WishlistConfig;
+use MediaWiki\Hook\ChangesListInsertArticleLinkHook;
+use MediaWiki\Page\PageReference;
+use MediaWiki\RecentChanges\ChangesList;
+use MediaWiki\RecentChanges\RecentChange;
+use MediaWiki\Title\Title;
+use MediaWiki\Title\TitleFormatter;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Hook handlers involving the appearance of entity pages in change lists such as recent changes and the watchlist.
+ */
+class ChangesListHooks implements ChangesListInsertArticleLinkHook {
+	use WishlistEntityTrait;
+
+	public function __construct(
+		private readonly WishlistConfig $config,
+		private readonly WishStore $wishStore,
+		private readonly FocusAreaStore $focusAreaStore,
+		private readonly TitleFormatter $titleFormatter,
+		private readonly LoggerInterface $logger,
+	) {
+	}
+
+	/**
+	 * Customize article links for wish and focus area pages in change lists.
+	 *
+	 * @param ChangesList $changesList
+	 * @param string &$articlelink HTML of link to page.
+	 * @param string &$s HTML of row that is being constructed.
+	 * @param RecentChange $rc
+	 * @param bool $unpatrolled
+	 * @param bool $watched
+	 */
+	public function onChangesListInsertArticleLink(
+		$changesList, &$articlelink, &$s, $rc, $unpatrolled, $watched
+	): void {
+		$pageRef = $rc->getPage();
+		if ( $this->config->isEnabled() && $this->config->isWishOrFocusAreaPage( $pageRef ) ) {
+			'@phan-var PageReference $pageRef';
+			$title = Title::newFromPageReference( $pageRef );
+			$entity = $this->getMaybeCachedEntity( $title, $changesList->getLanguage()->getCode() );
+			if ( !$entity ) {
+				$this->logger->error( __METHOD__ . ": Could not load entity for page {$title->toPageIdentity()}" );
+				return;
+			}
+			$articlelink = $this->getEntityLink( $entity, $changesList );
+		}
+	}
+}
---
+++ tests/phpunit/unit/ChangesListHooksTest.php
@@ -0,0 +1,89 @@
+<?php
+declare( strict_types = 1 );
+
+namespace MediaWiki\Extension\CommunityRequests\Tests\Unit;
+
+use MediaWiki\Extension\CommunityRequests\FocusArea\FocusAreaStore;
+use MediaWiki\Extension\CommunityRequests\HookHandler\ChangesListHooks;
+use MediaWiki\Extension\CommunityRequests\Wish\Wish;
+use MediaWiki\Extension\CommunityRequests\Wish\WishStore;
+use MediaWiki\Language\Language;
+use MediaWiki\RecentChanges\ChangesList;
+use MediaWiki\RecentChanges\RecentChange;
+use MediaWiki\Tests\Unit\FakeQqxMessageLocalizer;
+use MediaWiki\Title\TitleFormatter;
+use MediaWiki\User\UserIdentity;
+use MediaWikiUnitTestCase;
+use MockTitleTrait;
+use Psr\Log\NullLogger;
+
+/**
+ * @group CommunityRequests
+ * @covers \MediaWiki\Extension\CommunityRequests\HookHandler\ChangesListHooks
+ * @covers \MediaWiki\Extension\CommunityRequests\HookHandler\WishlistEntityTrait
+ */
+class ChangesListHooksTest extends MediaWikiUnitTestCase {
+
+	use MockWishlistConfigTrait;
+	use MockTitleTrait;
+
+	public function testOnChangesListInsertArticleLink(): void {
+		$wishTitle = $this->makeMockTitle( 'Community Wishlist/W1' );
+		$wish = new Wish(
+			$wishTitle,
+			'en',
+			$this->createNoOpMock( UserIdentity::class ),
+			[ Wish::PARAM_TITLE => 'Test wish title' ]
+		);
+		$wishStore = $this->createNoOpMock( WishStore::class, [ 'get' ] );
+		$wishStore->expects( $this->once() )
+			->method( 'get' )
+			->willReturn( $wish );
+
+		$titleFormatter = $this->createNoOpMock( TitleFormatter::class, [ 'getFullText' ] );
+		$titleFormatter->expects( $this->once() )
+			->method( 'getFullText' )
+			->willReturn( 'Community Wishlist/W1' );
+
+		$handler = new ChangesListHooks(
+			$this->getConfig(),
+			$wishStore,
+			$this->createNoOpMock( FocusAreaStore::class ),
+			$titleFormatter,
+			new NullLogger(),
+		);
+
+		$changesList = $this->createNoOpMock( ChangesList::class, [ 'getLanguage', 'msg' ] );
+		$changesList->expects( $this->once() )
+			->method( 'getLanguage' )
+			->willReturn( $this->createConfiguredMock( Language::class, [
+				'getCode' => 'en',
+			] ) );
+		$changesList->expects( $this->once() )
+			->method( 'msg' )
+			->willReturnCallback( [ new FakeQqxMessageLocalizer(), 'msg' ] );
+
+		$rc = $this->createNoOpMock( RecentChange::class, [ 'getPage' ] );
+		$rc->expects( $this->once() )
+			->method( 'getPage' )
+			->willReturn( $wishTitle->toPageIdentity() );
+
+		$articleLink = '';
+		$s = '';
+		$handler->onChangesListInsertArticleLink(
+			$changesList,
+			$articleLink,
+			$s,
+			$rc,
+			false,
+			false
+		);
+
+		$this->assertSame(
+			'<a title="Test wish title"><span class="ext-communityrequests-entity-link--label">Test wish title</span>' .
+			' <span class="mw-title ext-communityrequests-entity-link--id" style="font-size: 0.85em;">' .
+			'(parentheses: Community Wishlist/W1)</span></a>',
+			$articleLink
+		);
+	}
+}

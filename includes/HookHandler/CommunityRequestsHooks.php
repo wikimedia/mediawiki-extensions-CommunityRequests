e93fcb0d65b7bec7d403a1ac4d86df87fd0e819b
Jon Robson <jdlrobson@gmail.com>
2026-02-11 08:09:33 -0800

    Remove modification of namespaces menu
    The FIXME comment says to remove when
    T412238 is resolved and that has now happened
    
    Bug: T416652
    Change-Id: Ib1423e4e9b8fd190154fd517fbf57db391ef84dd
    Follow-Up: Ia9ae23b684bc3e1ebb7a354df75f2ce602884f97

--- includes/HookHandler/CommunityRequestsHooks.php
+++ includes/HookHandler/CommunityRequestsHooks.php
@@ -379,13 +379,6 @@ class CommunityRequestsHooks implements
 			: $talkPage->getLinkURL( [ 'action' => 'edit', 'redlink' => 1 ] );
 		$class = $talkPage->exists() ? '' : 'new';
 
-		// Skins build their links from different sources, so we need to set them all.
-		// @fixme: can remove this block when T412238 is resolved.
-		if ( isset( $links['namespaces']['talk'] ) ) {
-			$links['namespaces']['talk']['href'] = $href;
-			$links['namespaces']['talk']['class'] = $class;
-			$links['namespaces']['talk']['link-class'] = $class;
-		}
 		$links['associated-pages']['talk']['href'] = $href;
 		$links['associated-pages']['talk']['class'] = $class;
 		$links['associated-pages']['talk']['link-class'] = $class;
